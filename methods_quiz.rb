8510740fcfb34827e70b980a320f487b0826f021
David Sanders <sandersda@s.dcsdk12.org>
2017-10-02 10:56:39 -0600

    Implement icy_hot?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,7 +1,7 @@
   
 # TODO - write has_teen?
 def has_teen?(int1,int2,int3)
-	if int1 <= 19 and int1 >= 13 or int2 <= 19 and int2 >= 13 or int3 <= 19 and int3 >= 13
+	if (int1 <= 19 and int1 >= 13) or (int2 <= 19 and int2 >= 13) or (int3 <= 19 and int3 >= 13)
 		return true
 	end
 	return false
@@ -18,7 +18,10 @@ end
 # TODO - write icy_hot?
 
 def icy_hot?(temp1,temp2)
-
+	if (temp1 < 0 and temp2 > 100) or (temp1 > 100 and temp2 < 0)
+		return true
+	end
+	return false
 end
 
 
